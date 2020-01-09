54919a5e6a7fa3b3c191a13500377243b3e96157
Mazen Kasser <mazen.kasser@anz.com>
2020-01-09 13:52:24 +1300

    Markup cheatsheet

---
+++ Style Cheetsheet.playground/Contents.swift
@@ -0,0 +1,24 @@
+/*:
+ **MARKUP CHEETSHEET**
+ - - -
+ ![image title](image.png)
+ # heading 1
+ ## heading 2
+ ### heading 3
+ **bold** or __string__\
+ *italic* or _string_\
+ `highlighted`\
+ //\: special charachters
+ * | + | - bulletPoint
+ ```
+ var ouput
+ for i in input {
+ output.append(transform(i))
+ }
+ ```
+ > To insert a video
+ ![alternate text](video name poster="image file name" width="integer width" height="integer height")
+ Check out the full list [here!](https://developer.apple.com/library/content/documentation/Xcode/Reference/xcode_markup_formatting_ref/AddingMarkup.html#//apple_ref/doc/uid/TP40016497-CH100-SW1)
+ */
+//: [Go To Page Name]()\
+//: [Previous page](@previous) - [Next page](@next)
